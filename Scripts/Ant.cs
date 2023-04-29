3919ac2559d6bf7d6db94342a31737b51df7b819
OldSinner <antylifepatia@gmail.com>
2023-04-29 23:55:03 +0200

    Food Follow

--- Scripts/Ant.cs
+++ Scripts/Ant.cs
@@ -5,12 +5,22 @@ public class Ant : MonoBehaviour
     public float MaxSpeed;
     public float Speed;
     Vector2 Velocity;
-
+    private void Start()
+    {
+    }
     void Update()
     {
-        var target = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        var acc = target - (Vector2)transform.position;
+        HaveFood();
+        Move();
+    }
 
+    private void Move()
+    {
+        
+        var target = FindFood();
+        Debug.Log(target);
+        var acc = target - (Vector2)transform.position;
+        
         acc = acc.normalized * Speed;
 
         Velocity += acc * Time.deltaTime;
@@ -20,5 +30,46 @@ public class Ant : MonoBehaviour
         var position = transform.position + ((Vector3)Velocity * Time.deltaTime);
         transform.position = position;
         transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+    }
+
+    void HaveFood()
+    {
+      
+    }
+    public Vector2 FindFood()
+    {
+        Collider2D[] buffer = new Collider2D[10];
+        Physics2D.OverlapCircleNonAlloc(transform.position, 5, buffer);
+        Vector3 characterToCollider;
+        float dot;
+        foreach (var col in buffer)
+        {
+            if (col != null)
+            {
+                if(col.CompareTag("Food"))
+                {
+                    Debug.Log("Found Something");
+                    characterToCollider = (col.transform.position - transform.position).normalized;
+                    dot = Vector3.Dot(characterToCollider, transform.right);
+                    if (dot >= Mathf.Cos(55))
+                    {
+                        return col.transform.position;
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+        }
+
+        return Vector2.zero;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, 5);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.right * 10);
     }
 }
