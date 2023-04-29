fe642de26a4b1600abb2dfff1ed3a1540fc24e67
OldSinner <antylifepatia@gmail.com>
2023-04-29 21:41:23 +0200

    Basic Ant

--- Ant/Ant.cs
+++ Ant/Ant.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Ant : MonoBehaviour
 {
-    public float Speed { get; set; }
-    // Start is called before the first frame update
+    public float MaxSpeed;
+    public float MaxSteerPower;
+    public float SteerPower;
+    public float Speed;
+    Vector2 Velocity;
+    Vector2 Position;
+
     void Start()
     {
     }
@@ -13,6 +19,13 @@ public class Ant : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up * Time.deltaTime;
+        var target = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+        var Acceleration = (Vector2)(target - transform.position).normalized * (Speed * Time.deltaTime);
+        Acceleration = Vector2.ClampMagnitude(Acceleration * SteerPower, MaxSteerPower);
+        Velocity += Acceleration;
+        Vector2.ClampMagnitude(Velocity, MaxSpeed);
+        Position += Velocity * Time.deltaTime;
+
+        transform.SetPositionAndRotation(Position, Quaternion.Euler(0, 0, 0));
     }
 }
