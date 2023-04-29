eba0d042722fa8feeb70e5712bcdf16b605dbc15
OldSinner <antylifepatia@gmail.com>
2023-04-29 23:12:35 +0200

    Folder Rename

--- Scripts/Ant.cs
+++ Scripts/Ant.cs
@@ -1,17 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Ant : MonoBehaviour
 {
     public float MaxSpeed;
     public float Speed;
     Vector2 Velocity;
-
-    void Start()
-    {
-    }
 
     void Update()
     {
@@ -23,7 +16,6 @@ public class Ant : MonoBehaviour
         Velocity += acc * Time.deltaTime;
         Velocity = Vector2.ClampMagnitude(Velocity, MaxSpeed);
         var angle = Mathf.Atan2(Velocity.y, Velocity.x);
-        Debug.Log(angle);
 
         var position = transform.position + ((Vector3)Velocity * Time.deltaTime);
         transform.position = position;
